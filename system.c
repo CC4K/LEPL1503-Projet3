fb1c19d3eff3fce52bc270aa63fc7b5d48b1b25b
HOIg3r <jacques.hogge@hotmail.com>
2022-03-15 09:29:05 +0100

    create systeme file + add fonction

---
+++ system.c
@@ -0,0 +1,19 @@
+//
+// Created by jacques on 15/03/22.
+//
+
+//Includes
+
+#include "headers/system.h"
+
+/**
+ *
+ * Add two vectors in a Galois Field 256
+ * @param symbol_1: the first symbol to add
+ * @param symbol_2: the second symbol to add
+ * @param symbol_size: size of the two symbols (of the same size!)
+ * @return: a new vector of `symbol_size` byte containing the result of symbol_1 + symbol_2 in GF(256)
+ */
+uint8_t *gf_256_full_add_vector(uint8_t *symbol_1, uint8_t *symbol_2, uint32_t symbol_size){
+
+}
