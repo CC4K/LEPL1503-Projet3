a0ac0771d387faa1b4668ef726641707e86492f3
pierre <pierre.marcipont@student.uclouvain.be>
2022-03-15 10:20:59 +0100

    [Pierre] inv function

--- system.c
+++ system.c
@@ -27,6 +27,24 @@ uint8_t *gf_256_full_add_vector(uint8_t *symbol_1, uint8_t *symbol_2, uint32_t s
 
 /**
  *
+ * Divide a vector in a Galois Field 256 by a coefficient
+ * @param symbol: the symbol to add
+ * @param coef: the dividing coefficient
+ * @param symbol_size: size of the two symbols (of the same size!)
+ * @return: a new vector of `symbol_size` byte containing the result of symbol / coef in GF(256)
+ */
+uint8_t *gf_256_inv_vector(uint8_t *symbol, uint8_t coef, uint32_t symbol_size) {
+    //Fait par Pierre le 15/03/22
+    //TODO: Verifier si le code est bon
+    uint8_t *output = symbol;
+    for (int i = 0; i < symbol_size / sizeof(int); ++i) {
+        output[i] = f256_mul_table[(int) symbol[i]][1/coef];
+    }
+    return output;
+}
+
+/**
+ *
  * Multiply a vector by a coefficient in a Galois Field 256
  * @param symbol: the symbol to multiply
  * @param coef: the coefficient of the scaling
