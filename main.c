f8a7ed701ae75f587213ffe06defc87b9dc66e6e
HOIg3r <jacques.hogge@student.uclouvain.be>
2022-06-21 00:23:44 +0200

    test

--- main.c
+++ main.c
@@ -264,7 +264,7 @@ int main(int argc, char* argv[]) {
         strcat(full_path, directory_entry->d_name);
 
         // Setup verbose global variable
-        verbose = args.verbose;
+        //verbose = args.verbose;
 
         //======================================= Open input file ====================================================//
         input_file = fopen(full_path, "r");
